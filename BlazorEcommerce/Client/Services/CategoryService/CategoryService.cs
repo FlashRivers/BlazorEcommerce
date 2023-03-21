6e8effa85b1e5eacf2f2b11c2188ebb182a9a8f9
rick <rick@richardgarrison.com>
2023-03-21 07:46:26 -0400

    Mod 165

--- BlazorEcommerce/Client/Services/CategoryService/CategoryService.cs
+++ BlazorEcommerce/Client/Services/CategoryService/CategoryService.cs
@@ -1,4 +1,6 @@
-﻿namespace BlazorEcommerce.Client.Services.CategoryService
+﻿using BlazorEcommerce.Shared;
+
+namespace BlazorEcommerce.Client.Services.CategoryService
 {
     public class CategoryService : ICategoryService
     {
@@ -9,12 +11,57 @@
             _http = http;
         }
         public List<Category> Categories { get; set; } = new List<Category>();
+        public List<Category> AdminCategories { get; set; } = new List<Category>();
+
+        public event Action OnChange;
+
+        public async Task AddCategory(Category category)
+        {
+            var response = await _http.PostAsJsonAsync("api/category/admin", category);
+            AdminCategories = (await response.Content
+                .ReadFromJsonAsync<ServiceResponse<List<Category>>>()).Data;
+            await GetCategories();
+            OnChange.Invoke();
+        }
+
+        public Category CreateNewCategory()
+        {
+            var newCategory = new Category { IsNew = true, Editing = true };
+            AdminCategories.Add(newCategory);
+            OnChange.Invoke();
+            return newCategory;
+        }
+
+        public async Task DeleteCategory(int categoryId)
+        {
+            var response = await _http.DeleteAsync($"api/category/admin/{categoryId}");
+            AdminCategories = (await response.Content
+                .ReadFromJsonAsync<ServiceResponse<List<Category>>>()).Data;
+            await GetCategories();
+            OnChange.Invoke();
+        }
+
+        public async Task GetAdminCategories()
+        {
+            var result = await _http.GetFromJsonAsync<ServiceResponse<List<Category>>>("api/category/admin");
+            if (result != null && result.Data != null)
+                AdminCategories = result.Data;
+        }
 
         public async Task GetCategories()
         {
             var result = await _http.GetFromJsonAsync<ServiceResponse<List<Category>>>("api/category");
             if (result != null && result.Data != null)
                 Categories = result.Data;
+        }
+
+        public async Task UpdateCategory(Category category)
+        {
+            var response = await _http.PutAsJsonAsync("api/category/admin", category);
+            AdminCategories = (await response.Content
+                .ReadFromJsonAsync<ServiceResponse<List<Category>>>()).Data;
+            await GetCategories();
+            OnChange.Invoke();
         }
     }
 }
---
+++ BlazorEcommerce/Server/Migrations/20230321104720_FixCategoryColumnName.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorEcommerce.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixCategoryColumnName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Visibility",
+                table: "Categories",
+                newName: "Visible");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Visible",
+                table: "Categories",
+                newName: "Visibility");
+        }
+    }
+}
